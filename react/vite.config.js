3a2a8cc0ae82c7d8cc0065e867e59ba4170c6d19
Intencione <lafiteella@gmail.com>
2026-02-11 13:12:11 -0800

    update on react project and index.html

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'./',
-  build: {
-    assetsDir: './'
-  }
+  base: '/web-development-projects/react-app/',
 })
